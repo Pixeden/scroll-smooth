34367f0f5cc648799077324ebc731aeff1278660
Lionel T <elrumordelaluz@hotmail.com>
2017-10-17 22:30:11 +0200

    Make redeable function

--- src/anchorScroll.js
+++ src/anchorScroll.js
@@ -3,18 +3,17 @@ import scrollSmooth from './scrollSmooth'
 export default (query = '[href^="#"]:not([href="#"]', config) => {
   const links = document.querySelectorAll(query)
 
+  const handler = e => {
+    e.preventDefault()
+    const dest = document.getElementById(e.target.hash.substring(1))
+    if (!dest) return
+    scrollSmooth(dest, {
+      ...config,
+      callback: a => console.log(e.target)
+    })
+  }
+
   Array.from(links).map(link => {
-    link.addEventListener(
-      'click',
-      e => {
-        e.preventDefault()
-        const dest = document.getElementById(e.target.hash.substring(1))
-        if (!dest) return
-        scrollSmooth(dest, {
-          ...config,
-        })
-      },
-      false
-    )
+    link.addEventListener('click', handler, false)
   })
 }
