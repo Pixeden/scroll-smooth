c7c20084ede257fd23b551e69249026371469cba
Lionel T <elrumordelaluz@hotmail.com>
2017-10-19 21:22:56 +0200

    [braking] Change anchorScroll params. Made more flexible API

--- src/anchorScroll.js
+++ src/anchorScroll.js
@@ -1,15 +1,21 @@
 import scrollSmooth from './scrollSmooth'
 
-export default (query = '[href^="#"]:not([href="#"]', config) => {
+export default (
+  {
+    query = '[href^="#"]:not([href="#"]',
+    match = e => e.target.hash.substring(1),
+    config,
+    spy = false,
+  } = {}
+) => {
   const links = document.querySelectorAll(query)
-
   const handler = e => {
     e.preventDefault()
-    const dest = document.getElementById(e.target.hash.substring(1))
+    const dest = document.getElementById(match(e))
     if (!dest) return
     scrollSmooth(dest, {
       ...config,
-      callback: a => console.log(e.target)
+      callback: () => {},
     })
   }
 
