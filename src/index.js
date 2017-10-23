8c6655a9ccf7ea05577790b5940344280a80e7a6
Lionel T <elrumordelaluz@hotmail.com>
2017-10-23 15:35:20 +0200

    Add observer into index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import scrollSmooth from './scrollSmooth'
 import { calcEndPoint } from './tools'
 import anchorScroll from './anchorScroll'
+import observe from './observe'
 
 export default {
   to: scrollSmooth,
   calcEndPoint,
   anchorScroll,
+  observe,
 }
